af25372f697489210e847455c8cc6f92b40fd396
Miracle Lawrence <lawrencemiracle71@gmail.com>
2022-08-19 21:22:58 +0100

    finish

--- main.py
+++ main.py
@@ -1,18 +1,61 @@
+import time
 from turtle import Turtle, Screen
-from paddle import Paddle
+from paddle import Paddle, Ball, ScoreBoard
 
-paddle = Paddle()
+
+gameon = True
+
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+score = ScoreBoard()
+
 screen = Screen()
+winning_score = 10
 
 screen.title("Hit the damn ball!!!")
 screen.setup(800, 600)
 screen.bgcolor("black")
 
-
 screen.listen()
-screen.onkey(paddle.up, "Up")
-screen.onkey(paddle.down, "Down")
-screen.onkeypress(paddle.up, "Up")
-screen.onkeypress(paddle.down, "Down")
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkeypress(r_paddle.up, "Up")
+screen.onkeypress(r_paddle.down, "Down")
+
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+screen.onkeypress(l_paddle.up, "w")
+screen.onkeypress(l_paddle.down, "s")
+
+speed = 0.1
+while gameon:
+    time.sleep(speed)
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bouncey()
+
+    # detect collision with the paddle
+    if ball.distance(r_paddle) < 100 and ball.xcor() > 320 or ball.distance(l_paddle) < 100 and ball.xcor() < -320:
+        speed -= 0.009
+        time.sleep(speed)
+        ball.bouncex()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        time.sleep(0.1)
+        sc = score.add_score_left()
+        if sc == winning_score:
+            gameon = False
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        time.sleep(0.1)
+        sc = score.add_score_right()
+        if sc == winning_score:
+            gameon = False
+
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,14 +1,15 @@
 from turtle import Turtle
 import time
 
+
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.shape("square")
         self.color("white")
         self.penup()
         self.shapesize(10, 2)
-        self.goto(350, 0)
+        self.goto(position)
 
     def up(self):
         new_y = self.ycor() + 20
@@ -19,3 +20,69 @@ class Paddle(Turtle):
         new_y = self.ycor() - 20
         if new_y > -210:
             self.goto(self.xcor(), new_y)
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        time.sleep(1)
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bouncey(self):
+        self.y_move *= -1
+
+    def bouncex(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bouncex()
+
+
+class ScoreBoard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.color("white")
+        self.right = 0
+        self.left = 0
+        self.max = 10
+        self.goto(0, 280)
+        self.write(f"Left  {self.left}: {self.right} Right")
+
+    def add_score_left(self):
+        self.left += 1
+        self.goto(0, 280)
+        self.clear()
+        self.write(f"Left  {self.left}: {self.right} Right")
+        return self.left
+
+    def add_score_right(self):
+        self.right += 1
+        self.goto(0, 280)
+        self.clear()
+        self.write(f"Left  {self.left}: {self.right} Right")
+        return self.right
+
+    def gameover(self):
+        if self.right == self.max:
+            self.goto(0, 0)
+            self.clear()
+            self.write(f"Left  {self.left}: {self.right} Right \n GAMEOVER \n Player Right Wins")
+
+        if self.left == self.max:
+            self.goto(0, 0)
+            self.clear()
+            self.write(f"Left  {self.left}: {self.right} Right \n GAMEOVER \n Player Left Wins")
+
